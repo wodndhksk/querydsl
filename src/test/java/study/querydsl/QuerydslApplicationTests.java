b0754ae0e36fd2311f66f0f7cab7a72d8c9f50e8
Jaewoo Kim <wodndhksk@gmail.com>
2023-10-08 16:05:57 +0900

    Querydsl 설정과 검증
    - spring ver 3. 대 이상부터는 설정이 다름 (gradle 주석 참고)
    - build.gradle 에 generated 폴더 경로에 QClass 생성 세팅 (!!주의!! gitignore 에 추가할것) -> 테스트로 Q타입 클래스를 못찾는 오류로 인하여 generated 폴더 하위로 경로 설정함.
    - 테스트로 검증 (querydsl 세팅, lombok 검증용 테스트 코드)

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,42 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
 
+	@Autowired
+	EntityManager em;
+
+	// 프로젝트 생성 후 라이브러리 검증 테스트
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+
+//		QHello qHello = new QHello("h");
+	    QHello qHello = QHello.hello; //위와 같음
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello); // querydsl 설정 검증
+		assertThat(result.getId()).isEqualTo(hello.getId()); // lombok 검증
+
 	}
 
 }
