97d76cdc861e7f0a748c97d4140eb550264788ff
Jaewoo Kim <wodndhksk@gmail.com>
2023-10-18 22:14:24 +0900

    조인 - 기본 조인
    - 내부 조인
    - 세타 조인

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -189,4 +189,44 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
+
+    /**
+     * 팀 A에 소속된 모든 회원
+     */
+    @Test
+    public void join() {
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username") // extracting : 특정 필드 추출 (username)
+                .containsExactly("member1", "member2");
+        //  containsOnly: 순서, 중복을 무시하는 대신 원소값과 갯수가 정확히 일치
+        //  containsExactly: 순서를 포함해서 정확히 일치
+
+    }
+
+    @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
+
+
+
 }
